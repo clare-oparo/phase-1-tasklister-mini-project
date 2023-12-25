9330f9eda38569fe1d71c84c576281bce186ae75
Clare <clare.oparo@student.moringaschool.com>
2023-12-25 15:24:29 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault()
+    addToDos(e.target.new_task_description.value)
+    
+  })
 });
+
+function addToDos(toDo){
+  
+  let p = document.createElement("p")
+  let btn = document.createElement("button")
+  btn.textContent = " x "
+  p.textContent = `${toDo}  `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#tasks').appendChild(p)
+}
+
